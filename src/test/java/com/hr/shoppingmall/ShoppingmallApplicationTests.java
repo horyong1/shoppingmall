6b085ff21d5b6b64e68ad1a4b8fead301f033239
potato <107034858+horyong1@users.noreply.github.com>
2024-11-08 14:29:52 +0900

    first commit

---
+++ src/test/java/com/hr/shoppingmall/ShoppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
