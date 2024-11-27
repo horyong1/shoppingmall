0610205362e171941fb6b6ef5153db35933746b1
potato <107034858+horyong1@users.noreply.github.com>
2024-11-27 17:45:38 +0900

    찜목록 상품 브랜드 토글 버튼 생성

--- src/main/java/com/hr/shoppingmall/consumer/controller/RestConsumerController.java
+++ src/main/java/com/hr/shoppingmall/consumer/controller/RestConsumerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.hr.shoppingmall.common.dto.RestResponseDto;
 import com.hr.shoppingmall.consumer.dto.ConsumerDto;
 import com.hr.shoppingmall.consumer.service.ConsumerService;
+import com.hr.shoppingmall.shop.service.ShopService;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -14,7 +15,10 @@ import jakarta.servlet.http.HttpSession;
 @RequestMapping("api/consumer")
 public class RestConsumerController {
 
-
+    @Autowired
+    private ConsumerService consumerService;
+    @Autowired
+    private ShopService shopService;
     // 세션 로그인 체크
     @RequestMapping("getSessionConsumerInfo")
     public RestResponseDto getSessionConsumerInfo(HttpSession session){
@@ -32,5 +36,42 @@ public class RestConsumerController {
         restResponseDto.add("consumerNo", consumerInfo.getConsumerNo());
 
         return restResponseDto;
+    }
+
+    // 상품 찜 목록
+    @RequestMapping("productWishList")
+    public RestResponseDto productWishList(HttpSession session){
+        RestResponseDto restResponseDto = new RestResponseDto();
+        ConsumerDto consumerInfo = (ConsumerDto)session.getAttribute("consumerInfo");
+        
+        if(consumerInfo == null){
+            restResponseDto.setResult("fail");
+            restResponseDto.setReason("인증X");
+        
+            return restResponseDto;
+        }
+        
+        restResponseDto.add("productWishList", shopService.getWishlist(consumerInfo.getConsumerNo()));
+        
+        
+        return restResponseDto;
+    }
+
+    // 브랜드 찜 목록
+    @RequestMapping("sellerWishList")
+    public RestResponseDto sellerWishList(HttpSession session){
+        RestResponseDto restResponseDto = new RestResponseDto();
+        ConsumerDto consumerInfo = (ConsumerDto)session.getAttribute("consumerInfo");
+        
+        if(consumerInfo == null){
+            restResponseDto.setResult("fail");
+            restResponseDto.setReason("인증X");
+        
+            return restResponseDto;
+        }
+
+        restResponseDto.add("sellerWishList", shopService.getWishlistByConsumerNo(consumerInfo.getConsumerNo()));
+        
+        return restResponseDto; 
     }
 }
